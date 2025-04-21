5b009ab1f10d528cb1c9b7da996b5b9ea08c87bd
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-21 17:30:05 -0500

    feat(footer): add footer with attributions.

---
+++ src/public/components/footer-content.component.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "footer-content"
+}
+</script>
+
+<template>
+  <div class="grid bg-primary mt-4 p-4 align-content-start">
+    <div class="col-12 ml-3 align-items-center justify-content-center">
+      <p>Copyright &copy; 2025. ACME Studio</p>
+    </div>
+    <div class="col-12 ml-3 mt-3 align-items-center justify-content-center">
+      <p>
+        {{ $t('authoring-phrase.intro')}} <i class="pi pi-heart"/>
+        {{ $t('authoring-phrase.use')}} <a href="https://primevue.org/" target="_blank">PrimeVue</a>
+        {{ $t('authoring-phrase.author', { brand: 'ACME'})}}
+      </p>
+      <p>{{ $t('footer.powered-by')}} <a href="https://www.newsapi.org" target="_blank">NewsAPI.org</a> {{ $t('footer.and')}} <a href="https://www.clearbit.com" target="_blank">Clearbit Logo API</a> </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
