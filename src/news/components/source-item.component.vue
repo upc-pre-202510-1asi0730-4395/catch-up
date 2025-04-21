3c25c70d29c7f147a32a463db9587e21c720757a
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-21 18:35:31 -0500

    feat(sources): add source item component.

---
+++ src/news/components/source-item.component.vue
@@ -0,0 +1,33 @@
+<script>
+import {Source} from "../model/source.entity.js";
+
+export default {
+  name: "source-item",
+  emits: ['source-selected'],
+  props: { source: { type: Source,  required: true } },
+  methods: {
+    onSourceSelected() {
+      this.$emit('source-selected', this.source);
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="m-4">
+    <div @click="onSourceSelected()" class="flex align-content-start flex-wrap">
+      <span class="flex lg:align-items-center lg:justify-content-center mr-2">
+        <pv-avatar :aria-label="source.name"
+                   :image="source.urlToLogo"
+                   shape="circle"/>
+      </span>
+      <span class="flex align-items-center justify-content-center">
+        {{ source.name }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
