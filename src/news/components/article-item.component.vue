48328ed8ff877685aa1b4cd8d0cc7999f98570fb
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-23 12:31:01 -0500

    feat(articles): complete article item component structure and styles.

--- src/news/components/article-item.component.vue
+++ src/news/components/article-item.component.vue
@@ -31,9 +31,62 @@ export default {
 </script>
 
 <template>
-
+  <pv-card class="m-2">
+    <template #header>
+      <img :alt="article.title" :src="article.urlToImage" class="image-fit"/>
+    </template>
+    <template #title>
+      <p>{{ article.title}}</p>
+    </template>
+    <template #subtitle>
+      <p class="flex align-content-start flex-wrap">
+        <span class="flex align-items-center justify-content-center">
+          <pv-avatar :aria-label="article.source.name"
+                     :image="article.source.urlToLogo"
+                     shape="circle"/>
+        </span>
+        <span class="flex align-items-center justify-content-center">
+          {{ article.source.name }}
+        </span>
+      </p>
+      <p class="flex align-content-start flex-wrap">
+        <span class="text-sm">
+          {{ article.getFormattedPublishedAt() }}
+        </span>
+      </p>
+    </template>
+    <template #content>
+      <p class="flex align-content-start flex-wrap">
+        {{ article.description }}
+      </p>
+    </template>
+    <template #footer>
+      <div class="flex justify-content-center -webkit-box-sizing: border-box; justify-content: space-between;">
+        <a :href="article.url" target="_blank">{{ $t('read-more')}}</a>
+        <span class="p-spacer"></span>
+        <pv-select-button  :label="$t('article.share')" 
+                           icon="pi pi-share-alt"
+                           class="p-button-text p-button-sm"
+                           @click="shareArticle"
+                           aria-label="Share article"
+                           pv-tooltip="$t('article.copy-to-clipboard')"/>
+      </div>
+    </template>
+  </pv-card>
 </template>
 
 <style scoped>
-
+  .p-button-sm {
+    font-size: 0.875rem;
+    padding:  0.5rem 1rem;
+  }
+  .p-spacer {
+    flex: 1 1 auto;
+  }
+  
+  .image-fit {
+    object-fit: cover;
+    width: 100%;
+    height: 100%;
+  }
 </style>
