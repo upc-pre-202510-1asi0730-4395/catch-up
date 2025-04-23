7b093312343e86bcfd621919c2fc4f0189e2cd7d
Angel Velasquez <pcisavel@upc.edu.pe>
2025-04-23 12:34:27 -0500

    feat(articles): add article list component.

---
+++ src/news/components/article-list.component.vue
@@ -0,0 +1,18 @@
+<script>
+import ArticleItem from "./article-item.component.vue";
+
+export default {
+  name: "article-list",
+  components: {ArticleItem},
+  props: { articles: { type: Array, required: true }}
+}
+</script>
+
+<template>
+  <div v-for="article in articles">
+    <article-item :article="article"/>
+  </div>
+</template>
+
+<style scoped>
+</style>
